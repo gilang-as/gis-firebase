219367428943b8283f7ca3b0ab4c0f074137a92b
Gilang Adi S <gilangas@outlook.com>
2019-05-28 21:39:37 +0700

    v1

---
+++ public/tra.js
@@ -0,0 +1,240 @@
+var db = firebase.database();
+var a;
+var latitude = firebase.database().ref('pengaturan');
+latitude.on('value', showData, showError);
+function showData(items){
+	var data = items.val();
+	var lat = parseFloat(data.latitude);
+	var lng = parseFloat(data.longitude);
+	var zoom = parseFloat(data.zoom);
+	var latlng = {lat : lat, lng: lng };
+	console.log(latlng);
+	a(lat, lng, zoom);
+}
+function showError(err){
+	//console.log(err.val()); 
+}
+
+var style=[
+  {
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#212121"
+      }
+    ]
+  },
+  {
+    "elementType": "labels.icon",
+    "stylers": [
+      {
+        "visibility": "off"
+      }
+    ]
+  },
+  {
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#757575"
+      }
+    ]
+  },
+  {
+    "elementType": "labels.text.stroke",
+    "stylers": [
+      {
+        "color": "#212121"
+      }
+    ]
+  },
+  {
+    "featureType": "administrative",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#757575"
+      }
+    ]
+  },
+  {
+    "featureType": "administrative.country",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#9e9e9e"
+      }
+    ]
+  },
+  {
+    "featureType": "administrative.land_parcel",
+    "stylers": [
+      {
+        "visibility": "off"
+      }
+    ]
+  },
+  {
+    "featureType": "administrative.locality",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#bdbdbd"
+      }
+    ]
+  },
+  {
+    "featureType": "poi",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#757575"
+      }
+    ]
+  },
+  {
+    "featureType": "poi.park",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#181818"
+      }
+    ]
+  },
+  {
+    "featureType": "poi.park",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#616161"
+      }
+    ]
+  },
+  {
+    "featureType": "poi.park",
+    "elementType": "labels.text.stroke",
+    "stylers": [
+      {
+        "color": "#1b1b1b"
+      }
+    ]
+  },
+  {
+    "featureType": "road",
+    "elementType": "geometry.fill",
+    "stylers": [
+      {
+        "color": "#2c2c2c"
+      }
+    ]
+  },
+  {
+    "featureType": "road",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#8a8a8a"
+      }
+    ]
+  },
+  {
+    "featureType": "road.arterial",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#373737"
+      }
+    ]
+  },
+  {
+    "featureType": "road.highway",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#3c3c3c"
+      }
+    ]
+  },
+  {
+    "featureType": "road.highway.controlled_access",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#4e4e4e"
+      }
+    ]
+  },
+  {
+    "featureType": "road.local",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#616161"
+      }
+    ]
+  },
+  {
+    "featureType": "transit",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#757575"
+      }
+    ]
+  },
+  {
+    "featureType": "water",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#000000"
+      }
+    ]
+  },
+  {
+    "featureType": "water",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#3d3d3d"
+      }
+    ]
+  }
+];
+
+function a(lat, lng, zoom) {
+	firebase.database().ref('/maps').on('value', function(snapshot) {
+    //console.log(snapshot.val());
+	var data = snapshot.val();
+	var result = [];
+
+	for (var key in data ){
+	   result = result.concat(data [key]);
+	}
+	$('#activmap-wrapper').activmap({
+        places: result,
+        icon: 'https://cdn.mapmarker.io/api/v1/pin?icon=fa-star&size=50&hoffset=0&voffset=-1',
+        lat: lat, //latitude of the center
+        lng: lng, //longitude of the center
+        posPanel: 'right',
+        showPanel: true,
+        radius: 0,
+		styles: style,
+        cluster: true,
+        country: 'id',
+        mapType: 'roadmap',
+        request: 'large',
+        allowMultiSelect: true,
+        locationTypes: ['geocode', 'establishment']
+    });
+	});
+};
+
+
+
+
+
+
+
+
+
